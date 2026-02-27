ae095776ecf71d7a77201b2f65d6ab3e62623409
CobhamZA <marc.geffroy@cobham.com>
2026-02-27 16:15:24 +0200

    Added new exercise

---
+++ ch7/ex_7_3.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+const int NUMBER_OF_EMPLOYEES = 3;
+
+int adjustDays(int oldDays);
+// Returns oldDays plus 5
+
+
+int main(){
+    using namespace std;
+
+    int vacation[NUMBER_OF_EMPLOYEES], number;
+
+    cout << "Enter allowed vacation days for employees 1"
+                << " through " << NUMBER_OF_EMPLOYEES << ":\n";
+    for(number = 1; number <= NUMBER_OF_EMPLOYEES; number++){
+        cin >> vacation[number - 1];
+    }
+
+    // The line below doesn't raise an error it falls over silently
+    // apon the return 0. The debugger raises an error 
+    // for(number = 1; number <= NUMBER_OF_EMPLOYEES; number++){
+    for(number = 0; number < NUMBER_OF_EMPLOYEES; number++){
+        vacation[number] = adjustDays(vacation[number]);
+    }
+    cout << "The revised number of vacation days are:\n";
+    for(number = 1; number <= NUMBER_OF_EMPLOYEES; number++){
+        cout << "Employee number " << number << " vacation days = " << vacation[number - 1] << endl;
+    }
+    return 0;
+}
+
+int adjustDays(int oldDays){
+    return (oldDays + 5);
+}
