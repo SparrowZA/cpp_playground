0633f148c95c6faa6616ce04a0cfb9e8d7455d99
SparrowZA <geffroymarc@gmail.com>
2026-02-28 08:33:14 +0200

    Added ex 6.6

--- ch6/ex_6_6.cpp
+++ ch6/ex_6_6.cpp
@@ -1,8 +1,50 @@
 #include <fstream>
 #include <iostream>
 #include <cstdlib>
-#include <iomanip>
+#include <iomanip>  // needed for setw
+// using namespace std;
 
+void makeNeat(std::ifstream& messyFile, std::ofstream& neatFile,
+        int numberAfterDecimalPoint, int filedWidth);
+        
 int main(){
+    std::ifstream fin;
+    std::ofstream fout;
+
+    fin.open("rawdata.dat");
+    if(fin.fail()){
+        std::cout << "Input file opening failed.\n";
+        exit(1);
+    }
+    fout.open("neat.dat");
+    if(fout.fail()){
+        std::cout << "Output file opening failed.\n";
+        exit(1);
+    }
+
+    makeNeat(fin, fout, 5, 12);
+
+    fin.close();
+    fout.close();
+
+    std::cout << "End program.\n";
     return 0;
+}
+
+void makeNeat(std::ifstream& messyFile, std::ofstream& neatFile,
+        int numberAfterDecimalPoint, int fieldWidth){
+    neatFile.setf(std::ios::fixed);
+    neatFile.setf(std::ios::showpoint);
+    neatFile.setf(std::ios::showpos);
+    neatFile.precision(numberAfterDecimalPoint);
+    std::cout.setf(std::ios::fixed);
+    std::cout.setf(std::ios::showpoint);
+    std::cout.setf(std::ios::showpos);
+    std::cout.precision(numberAfterDecimalPoint);
+
+    double next;
+    while(messyFile >> next){
+        std::cout << std::setw(fieldWidth) << next << std::endl;
+        neatFile << std::setw(fieldWidth) << next << std::endl;
+    }
 }
